de1147c62eba02f571032ce62a335b20a95a1a75
fiskejonas <jonasfiskekrogh@hotmail.com>
2024-02-27 15:24:37 +0100

    message

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,4 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [mdx(), tailwind()],
   site: 'https://fiskejonas.github.io',
-  base: '/production_portfolio3',
 });
